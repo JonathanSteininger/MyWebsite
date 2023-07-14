8693065c0101df34bf59d37d6538bab4a6e12452
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-07-14 18:52:38 +1200

    Added javascript to update body bottom margin when the footer changes height.

--- MyActualWebsite/wwwroot/js/site.js
+++ MyActualWebsite/wwwroot/js/site.js
@@ -1,4 +1,73 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+window.onload = PageLoaded;
+
+var footer;
+var PastHeight;
+
+
+/**
+ * runs when the page loads. 
+ * used to initialize variables.
+ * create event listeners.
+ * etc
+ */
+function PageLoaded() {
+    GetFooter();
+    window.addEventListener("resize", ResizeEvent);
+}
+
+/**
+ * runs when the page resizes.
+ * @param {any} evtArgs used the pass data about the resize event.
+ */
+function ResizeEvent(evtArgs) {
+    console.log(evtArgs);
+    FooterResizeCheck();
+}
+/**
+ * checks if the footer has changed size.
+ * if the footers height has changed, will update the body with a new bottom margin size equal to the footer size.
+ * 
+ * @returns exit statment
+ */
+function FooterResizeCheck() {
+    if (PastHeight == null) {
+        OnFooterSizeChanged();
+        return;
+    }
+    if (GetFooterHeight() != PastHeight) {
+        OnFooterSizeChanged();
+    }
+
+}
+/**
+ * updates elements based on the new footer size.
+ * 
+ * curretnly updated the bodies bottom margin to the footers height.
+ */
+function OnFooterSizeChanged() {
+    let size = footer.clientHeight;
+    document.body.style.marginBottom = size + "px";
+
+
+    PastHeight = GetFooterHeight();
+}
+
+/**
+ * return the footer's height
+ * 
+ * @returns footer's height
+ */
+function GetFooterHeight() {
+    if (footer == null) GetFooter();
+    return footer.clientHeight;
+}
+/**
+ * sets the footer from the page.
+ */
+function GetFooter() {
+    let list = document.getElementsByTagName("footer");
+    footer = list[0];
+    return footer;
+}
