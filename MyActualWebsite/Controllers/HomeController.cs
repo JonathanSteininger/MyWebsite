77234af1c5c45f19465282fce4b4b38624d358b9
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-07-18 00:23:21 +1200

    Updated the styling and everything on the portfolio page.

--- MyActualWebsite/Controllers/HomeController.cs
+++ MyActualWebsite/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyActualWebsite.Data;
 using MyActualWebsite.Models;
+using NuGet.Protocol;
 using System.Diagnostics;
 using System.IO;
 
@@ -60,11 +62,121 @@ namespace MyActualWebsite.Controllers
         {
             return View();
         }
-        public IActionResult Portfolio()
+        [Route("Home/Portfolio")]
+        [Route("Home/Portfolio/{tags?}")]
+        public IActionResult Portfolio(List<Tag>? tags)
         {
-            return View();
+            if (tags == null || tags.Count == 0)
+            {
+                return View(_context.Project.Include(x => x.Tags)
+                    .ThenInclude(x => x.TagCatagory)
+                    .ToList()
+                    .FindAll(item => CheckProjectTags(item, tags))
+                    );
+            }
+            List<Project> projects = new List<Project>();
+            if(_context != null && _context.Project != null)
+            {
+                projects = _context.Project.Include(x => x.Tags)
+                    .ThenInclude(x => x.TagCatagory)
+                    .ToList()
+                    .FindAll(item => CheckProjectTags(item, tags));
+            }
+            projects.Sort((a,b) => {
+                if (a.EndDate == null) return -1;
+                if (b.EndDate == null) return 1;
+                return a.EndDate.Value.CompareTo(b.EndDate.Value);
+                });
+            return View(projects);
+        }
+        /*
+        
+        [Route("Home/Portfolio")]
+        [Route("Home/Portfolio/{tags?}")]
+        public IActionResult Portfolio(string? tags)
+        {
+            List<Project> projects = new List<Project>();
+            if (_context != null && _context.Project != null)
+            {
+
+                projects = _context.Project.Include(x => x.Tags).ThenInclude(x => x.TagCatagory).ToList();
+            }
+            if (string.IsNullOrEmpty(tags))
+            {
+                return View(projects);
+            }
+            string[] TagsArray = tags.ToUpper().Split(',');
+
+            projects = projects.FindAll(item => filterProject(item, TagsArray));
+
+            projects.Sort((a, b) => {
+                if (a.EndDate == null) return -1;
+                if (b.EndDate == null) return 1;
+                return a.EndDate.Value.CompareTo(b.EndDate.Value);
+            });
+            return View(projects);
+        }
+        */
+        /*
+        public IActionResult Portfolio(params string[] tags)
+        {
+            List<Project> projects = new List<Project>();
+            if (_context != null && _context.Project != null)
+            {
+                projects = _context.Project.ToList();
+            }
+            if (tags == null || tags.Length == 0)
+            {
+                return View(projects);
+            }
+            NormalizeArray(ref tags);
+
+            projects = projects.FindAll(item => filterProject(item, tags));
+
+            projects.Sort((a, b) => {
+                if (a.EndDate == null) return -1;
+                if (b.EndDate == null) return 1;
+                return a.EndDate.Value.CompareTo(b.EndDate.Value);
+            });
+            return View(projects);
         }
 
+        private void NormalizeArray(ref string[] tags)
+        {
+            for(int i = 0; i < tags.Length; i++)
+            {
+                tags[i] = tags[i].ToUpper();
+            }
+        }
+        */
+
+        private bool filterProject(Project item, string[] tags)
+        {
+            string[] tagsCache = item.Tags.ConvertAll(item => item.TagName.ToUpper()).ToArray();
+            foreach (string tag in tags)
+            {
+                if (tag.Length > 0 && tag[0] == '-')
+                {
+                    string newTag = tag.Substring(1);
+                    if (tagsCache.Contains(newTag)) return false;
+                } 
+                if (!tagsCache.Contains(tag)) return false;
+
+            }
+            return true;
+        }
+
+        
+        private bool CheckProjectTags(Project project, List<Tag> tags)
+        {
+            if (tags == null) return true;
+            foreach (Tag tag in tags)
+            {
+                if(!project.Tags.Contains(tag)) return false;
+            }
+            return true;
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
