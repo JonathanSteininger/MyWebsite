e018576da65ce99d900dfd3949d3d20d42d3bb0f
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-07-17 20:33:50 +1200

    added featured option for projects.

---
+++ MyActualWebsite/Data/Migrations/20230717072924_featured-optiont-for-projects.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyActualWebsite.Data.Migrations
+{
+    public partial class featuredoptiontforprojects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Featured",
+                table: "Project",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Featured",
+                table: "Project");
+        }
+    }
+}
